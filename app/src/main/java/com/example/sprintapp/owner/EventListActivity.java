fcbc00960fb533fffda5721a4c63bf310844a10a
przechrzta <jacekprzechrzta@gmail.com>
2019-12-04 23:34:29 +0100

    asd

--- app/src/main/java/com/example/sprintapp/owner/EventListActivity.java
+++ app/src/main/java/com/example/sprintapp/owner/EventListActivity.java
@@ -2,7 +2,9 @@ package com.example.sprintapp.owner;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.sprintapp.R;
 import com.example.sprintapp.shared.Event;
 import com.example.sprintapp.shared.EventListAdapter;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
@@ -29,6 +32,7 @@ public class EventListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
 
+        onNavBarClick();
         Intent incomingIntent = getIntent();
         final String date = incomingIntent.getStringExtra("date");
         setTitle(date);
@@ -57,6 +61,34 @@ public class EventListActivity extends AppCompatActivity {
             }
 
         });
+    }
+
+    protected void onNavBarClick(){
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.calendar:
+                        finish();
+                        Intent intent = new Intent(getApplicationContext(), OwnerMainActivity.class);
+                        startActivity(intent);
+                        return true;
+
+                    case R.id.today:
+                        Intent intent1= new Intent(getApplicationContext(), EventListActivity.class);
+                        startActivity(intent1);
+                        return true;
+
+                    case R.id.manage:
+                        Intent intent2= new Intent(getApplicationContext(), ManageActivity.class);
+                        startActivity(intent2);
+                        return true;
+                }
+                return true;
+            }
+        });
+
     }
 
 }
--- app/src/main/java/com/example/sprintapp/owner/ManageActivity.java
+++ app/src/main/java/com/example/sprintapp/owner/ManageActivity.java
@@ -1,38 +1,23 @@
 package com.example.sprintapp.owner;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.CalendarView;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
 import com.example.sprintapp.R;
-import com.example.sprintapp.client.RegisterActivity;
-import com.example.sprintapp.shared.DateHelper;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class OwnerMainActivity extends AppCompatActivity implements CalendarView.OnDateChangeListener {
-
+public class ManageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.owner_main_activity);
+        setContentView(R.layout.activity_manage);
 
         onNavBarClick();
-
-        CalendarView calendar = findViewById(R.id.simpleCalendarView);
-        calendar.setOnDateChangeListener(this);
-    }
-
-    @Override
-    public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-        String date = DateHelper.format(year, month, dayOfMonth);
-        Intent intent = new Intent(getApplicationContext(), EventListActivity.class);
-        intent.putExtra("date", date);
-        startActivity(intent);
     }
 
     protected void onNavBarClick(){
