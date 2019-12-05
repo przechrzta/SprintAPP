f52348b983e353ebb8d8c692f86697699db3cd4b
Jacek Przechrzta <jacekprzechrzta@gmail.com>
2019-12-05 20:21:58 +0100

    Dodanie dzwonienia

--- app/src/main/java/com/example/sprintapp/shared/EventListAdapter.java
+++ app/src/main/java/com/example/sprintapp/shared/EventListAdapter.java
@@ -1,12 +1,17 @@
 package com.example.sprintapp.shared;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -35,12 +40,32 @@ public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.Even
     }
 
     @Override
-    public void onBindViewHolder(EventViewHolder holder, int position) {
+    public void onBindViewHolder(final EventViewHolder holder, int position) {
         final Event event = events.get(position);
 
         holder.clientNameTextView.setText(event.getClientName());
         holder.clientPhoneNumberTextView.setText(event.getPhoneNumber());
         holder.eventTypeTextView.setText(event.getEventType());
+
+        holder.clientDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(holder.context, EventDetailsActivity.class);
+                holder.context.startActivity(intent);
+            }
+        });
+
+        holder.callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + event.getPhoneNumber()));
+                if (holder.context.checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(holder.context, R.string.no_phone_permission, Toast.LENGTH_SHORT).show();
+                } else {
+                    holder.context.startActivity(intent);
+                }
+            }
+        });
     }
 
     @Override
@@ -55,22 +80,19 @@ public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.Even
         TextView clientPhoneNumberTextView;
         TextView eventTypeTextView;
         LinearLayout clientDetails;
+        Button callButton;
+
+        final Context context;
 
 
         EventViewHolder(View v, final Context context) {
             super(v);
+            this.context = context;
             clientNameTextView = v.findViewById(R.id.clientName);
             clientPhoneNumberTextView = v.findViewById(R.id.clientPhoneNumber);
             eventTypeTextView = v.findViewById(R.id.eventType);
-
             clientDetails = v.findViewById(R.id.clientDetails);
-            clientDetails.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(context, EventDetailsActivity.class);
-                    context.startActivity(intent);
-                }
-            });
+            callButton = v.findViewById(R.id.call_button);
         }
 
     }
